2a4a1e2c7027f54437a9c46251a2423aa440249b
Reegan McAllister <reegan@ualberta.ca>
2016-06-09 15:45:39 -0600

    update

--- Signal Decoder.py
+++ Signal Decoder.py
@@ -2,11 +2,16 @@
 
 import serial
 import numpy as np
+import matplotlib.pyplot as plt
 import time
 import json
 import sys
 import os
 import os.path
+
+
+sample_rate = 33
+
 
 #load previous serial settings are prompt for new ones
 settings_file = 'settings.json'
@@ -24,7 +29,7 @@ else:
     json.dump(serial_settings,f, indent=4)
     f.close()
 
-valid_chars = ['0' '1' '\n']
+valid_chars = ['0' '1' '\n'] #binary data
 
 def readlineCR(port):
     rv = b""
@@ -45,84 +50,104 @@ def readlineCR(port):
             print("invalid character received")
 
 #try:
-#s = serial.Serial(port, baudrate=baud, timeout=3.0)
+    s = serial.Serial(port, baudrate=baud, timeout=3.0)
 
-#receive string from arduino
-#string = readlineCR(port)
-string = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111100000000000000000000000000000000000000000000000000111111111100000000001111111111\n"
-#remove \n
-string = string[0:(len(string)-1)]
-#split characters
-bits = list(string)
-#convert to integers
-bits = [ int(x) for x in bits ]
+    #receive string from arduino
+    string = readlineCR(port)
+    #test string
+    #string = "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111110000000000000000000000000000000000000000000000000011111111110000000000111111111111100000000001111111111000000000011111111110000000000111111111100000000001111111111000000000011111111110000000000111111111100000000001111111111000000000011111111110000000000111111111100000000001111111111111111111100000000001111111111000000000011111111110000000000000000000000000000000000000000111111111100000000001111111111000000000011111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111100000000000000000000000000000000000000000000000000111111111100000000001111111111000000000011111111110000000000111111111100000000001111111111000000000011111111110000000000111111111100000000001111111111000000000011111111110000000000111111111100000000001111111111000000000011111111111111111111000000000011111111110000000000\n"
 
-#find length of each bit
-a = np.array([], dtype=int)
-hold  = bits[0]
-c = 0
-for m in bits:
-    if m == 0:
-        if hold == 0:
-            c += -1
-        if hold == 1:
-            hold = 0
-            a = np.append(a,[c])
-            c = -1
-    else:
-        if hold == 1:
-            c += 1
-        if hold == 0:
-            hold = 1
-            a = np.append(a,[c])
-            c = 1
-a = np.append(a,[c])
-#reduce
-a = a / np.min(np.abs(a))
-#numpy rounds 0.5's to the nearest even number
-#this might cause issues
-a = np.rint(a) 
+    #remove \n
+    string = string[0:(len(string)-1)]
 
-#build reduced data byte
-d = np.array([], dtype=int)
-for n in a:
-    if n < 0:
-        d = np.hstack((d,np.zeros(int(np.abs(n)),dtype=int)))
-    else:
-        d = np.hstack((d,np.ones(int(np.abs(n)),dtype=int)))
+    #split characters
+    bits = list(string)
 
-#convert to bytes
-numbytes = int(np.ceil(len(d)/8))
-#add zeros to make an even number of bytes
-while len(d) < numbytes*8:
-    d = np.append(d,[0])
+    #convert to integers
+    bits = [ int(x) for x in bits ]
 
-d = d.reshape((numbytes,8),order='C')
+    num_samples = len(bits)
 
-#flip order so LSb's are on right
-d = np.fliplr(d)
+    #find length of each "bit"
+    a = np.array([], dtype=int)
+    hold  = bits[0]
+    c = 0
+    for m in bits:
+        if m == 0:
+            if hold == 0:
+                c += -1 #count 0 as negative
+            if hold == 1:
+                hold = 0
+                a = np.append(a,[c])
+                c = -1
+        else:
+            if hold == 1:
+                c += 1 #count 1 as positive
+            if hold == 0:
+                hold = 1
+                a = np.append(a,[c])
+                c = 1
+    a = np.append(a,[c])
 
-#convert to list
-d = d.tolist()
+    #find length of single bit (shortest "bit" is a single bit)
+    bit_length = np.min(np.abs(a))
+    #reduce
+    a = a / bit_length
+    #numpy rounds 0.5's to the nearest even number
+    a = np.rint(a) 
 
-#convert to strings of bytes
-#this is a lot harder than it should be
-for k,row in enumerate(d):
-    for j,col in enumerate(row):
-        d[k][j] = str(d[k][j])
-for index,row in enumerate(d):
-    d[index] = ''.join(row)
+    #build reduced data byte
+    d = np.array([], dtype=int)
+    for n in a:
+        if n < 0:
+            d = np.hstack((d,np.zeros(int(np.abs(n)),dtype=int)))
+        else:
+            d = np.hstack((d,np.ones(int(np.abs(n)),dtype=int)))
 
-#convert binary to decimal number
-for b,bite in enumerate(d):
-    bite = '0b' + bite
-    d[b] = int(bite,2)
+    bits2 = d.tolist()
 
-#convert to hex
-for b,bite in enumerate(d):
-    d[b] = hex(bite)
+    #convert to bytes
+    numbytes = int(np.ceil(len(d)/8))
+    #add zeros to make an even number of bytes
+    while len(d) < numbytes*8:
+        d = np.append(d,[0])
 
-print(d)
+    d = d.reshape((numbytes,8),order='C')
 
-#except:
-    #print("something went wrong")
+    #flip order so LSb's are on right
+    d = np.fliplr(d)
+
+    #convert to list
+    d = d.tolist() 
+
+    #convert to strings of bytes
+    #this is a lot harder than it should be
+    for k,row in enumerate(d):
+        for j,col in enumerate(row):
+            d[k][j] = str(d[k][j])
+    for index,row in enumerate(d):
+        d[index] = ''.join(row)
+
+    #convert binary to decimal number
+    for b,bite in enumerate(d):
+        bite = '0b' + bite
+        d[b] = int(bite,2)
+
+    #convert to hex
+    for b,bite in enumerate(d):
+        d[b] = hex(bite)
+
+    #bit time
+    bit_time = bit_length*sample_rate
+    #this will always under estimate bit time :(
+    #to do: better frequency calculation
+
+    print('Bit time: ',bit_time)
+    print('Code: ',d)
+
+    plt.plot( list(range(0,num_samples*sample_rate,sample_rate)) , bits, list(range(0,len(bits2)*bit_time,bit_time)), bits2)
+    plt.axis([0,num_samples*sample_rate,0,1.5])
+    plt.show()
+
+except:
+    print("something went wrong")
